c29a6bf55d648597a792908472c9424d32df2705
enriqueperez21 <luis.perez05@epn.edu.ec>
2023-07-11 09:28:42 -0500

    CuponScreen Advance

--- app/screens/ScreenNav/Cupon.js
+++ app/screens/ScreenNav/Cupon.js
@@ -1,15 +1,36 @@
 import theme from "../../theme/theme";
+import ImagenImport from "../../theme/Images";
 import { StyleSheet, Text, View, Image, ScrollView, Dimensions } from 'react-native';
 import { HeadbarCanje } from "../../PruebaComponents/Headbar";
+
 
 export const CuponScreen  = () =>{
     return( 
     <View style={styles.container}>
         <HeadbarCanje/>
         <View style={styles.body}>
-            <Text>Prueba cupones</Text>
+            
+            {/* Mostrar Puntos  */}
+            <View style={styles.mostrarPuntos}>
+                <View style={styles.centrado}>
+                    <Text style={styles.tipoLetra}>Tus puntos:</Text>
+                </View>
+                
+                <View style={styles.puntos}>
+                    <Image
+                        source={ImagenImport.coin}
+                        style={{ width: 20, height: 20 }}
+                    />
+                    <Text style={styles.cantidadPuntos}>200</Text>
+                </View>
+            </View>
+            
+            {/* Mensaje */}
+            <View style={styles.centrado}>
+                    <Text style={styles.tipoLetra}>Ingresa el código proporcionado en nuestro restaurante:</Text>
+            </View>
+
         </View>
-        
     </View>)
 }
 const styles = StyleSheet.create({
@@ -24,4 +45,29 @@ const styles = StyleSheet.create({
         flex: 1,
         paddingHorizontal: theme.separation.horizontalSeparation,
     },
+    tipoLetra:{
+        fontFamily: theme.fonts.text,
+        fontSize: theme.fontSize.body,
+    },
+    centrado: {
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    mostrarPuntos:{
+        marginTop: 10,
+        flexDirection: "row",
+        justifyContent: "space-between",
+        height: 30,
+    },
+    puntos:{
+        backgroundColor: theme.colors.blackSegunda,
+        borderRadius: 50,
+        width: 80,
+        flexDirection: "row",
+        alignItems: 'center',
+        justifyContent: "space-evenly",
+    },
+    cantidadPuntos:{
+        color: theme.colors.whiteSegunda,
+    }
 });
---
+++ app/theme/Images.js
@@ -0,0 +1,7 @@
+import Icono_coin from '../../assets/Icons/coin.png'
+
+export const ImagenImport={
+    coin: Icono_coin,
+}
+export default ImagenImport
+
